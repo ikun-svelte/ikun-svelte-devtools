5c13e5411fe7619674a77cafc8ac4def25caf653
baiwusanyu-c <740132583@qq.com>
2023-09-01 18:38:39 +0800

    chore: updated package.json

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,16 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
-import svelteDevtools from '../packages/server/dist/index.js'
-import Inspect from 'vite-plugin-inspect'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import svelteDevtools from '@ikun-svelte-devtools/server';
+import Inspect from 'vite-plugin-inspect';
 export default defineConfig({
-  plugins: [
-    // ssr
-    svelteDevtools({
-      sveltekit: sveltekit(),
-    }),
-    Inspect(),
-  ],
-  server: {
-    port: 5174,
-  },
-})
+	plugins: [
+		// ssr
+		svelteDevtools({
+			sveltekit: sveltekit()
+		}),
+		Inspect()
+	],
+	server: {
+		port: 5174
+	}
+});
