be40ec277904aec1982ed3057ff54ad695535fa6
baiwusanyu-c <740132583@qq.com>
2023-09-05 17:36:15 +0800

    fix: avoid duplicate mount clients

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -6,7 +6,10 @@ import '@unocss/reset/tailwind.css';
 import 'uno.css';
 createSvelteToolsClient();
 const app = new App({
-	target: document.getElementById('app')
+	target: document.getElementById('app')!
 });
 
+(
+	window.parent as Window & { __SVELTE_DEVTOOLS_GLOBAL_HOOK__: any }
+).__SVELTE_DEVTOOLS_GLOBAL_HOOK__.emit('app:init');
 export default app;
